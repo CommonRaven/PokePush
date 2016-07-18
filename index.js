594643f2de0cf8b67cb51a1ce0168aab87b6f657
Nitai J. Perez <nitai.perez@gmail.com>
2016-07-18 12:11:01 +0300

    Added http server with /json endpoin for sampling

--- index.js
+++ index.js
@@ -5,16 +5,20 @@
  */
 'use strict';
 
+const http = require('http');
 const co = require('co');
 const later = require('later');
 
 const request = require('./request');
 const integrations = require('./integrations');
 
-var prevStatus = 'Online';
+var lastStatus = {
+    lastChange: Date.now(),
+    status: 'Online'
+};
 
-later.setInterval(tick, later.parse.recur().every(5).minute());
-tick();
+// later.setInterval(tick, later.parse.recur().every(5).minute());
+// tick();
 
 function getStatus(body) {
     var find = '<h2>Status:';
@@ -34,12 +38,34 @@ function tick() {
             console.warn(`failed fetching servers status: ${err}`);
         }
 
-        if (prevStatus === status) {
+        if (lastStatus.status === status) {
             console.log(' > no change.');
         } else {
-            console.log(`> status changed: ${prevStatus} -> ${status}`);
-            prevStatus = status;
-            return integrations.send(status, prevStatus);
+            console.log(`> status changed: ${lastStatus.status} -> ${status}`);
+            lastStatus.status = {
+                status: status,
+                lastChange: Date.now()
+            };
+            return integrations.send(status, lastStatus.status);
         }
     });
 }
+
+//We need a function which handles requests and send response
+function handleRequest(req, res) {
+
+    switch (req.url.toLowerCase()) {
+        case "/json":
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(lastStatus));
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end(`We don't know any ${req.url.toLowerCase()}, nothing to see here.`);
+    }
+}
+
+//Create a server
+http.createServer(handleRequest).listen(80, () => {
+    console.log("Server listening on 80");
+});
