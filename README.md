6b72f969711c8286b409a127e61f8b605c14c031
Nitai J. Perez <nitai.perez@gmail.com>
2016-07-19 14:08:20 +0300

    Updated PB integration

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This verion relies on https://status.pokemongoserver.com, and won't sample the s
   - pushbullet
 2. Set your integration key (an integration that is not configured will not be activated):
   - slack : set Webhook URL in an environment variable names `SLACK_URL`.
-  - pushbullet : set pushbullet device iden in an environment variable named `PUSHBULLET_KEY`.
+  - pushbullet : set pushbullet API key in an environment variable named `PB_API_KEY` and device iden  in `PB_DEVICE_IDEN`
 3. run `$ node index`
 
 Best prectice is to run using [PM2](https://www.npmjs.com/package/pm2):
@@ -20,5 +20,5 @@ $ git clone git@github.com:CommonRaven/PokePush.git
 $ cd PokePush
 $ npm i
 $ npm i -g pm2
-$ PUSHBULLET_KEY=your_key SLACK_URL=http://example.com pm2 start index.js
+$ PB_API_KEY=your_key PB_DEVICE_IDEN="" SLACK_URL="http://example.com pm2 start index.js"
 ```
--- integrations/push-bullet.js
+++ integrations/push-bullet.js
@@ -6,10 +6,12 @@
 var Promise = require('bluebird');
 var PushBullet = require('pushbullet');
 
-let key = process.env.PUSHBULLET_KEY;
+let key = process.env.PB_API_KEY;
+let device = process.env.PB_DEVICE_IDEN;
+
 let pusher;
 
-if (key) {
+if (key && device) {
     console.log(`Launching process with Pushbullet key: ${process.env.PUSHBULLET_KEY}`);
     pusher = Promise.promisifyAll(new PushBullet(key));
 }
@@ -18,7 +20,7 @@ module.exports = (currentStatus, previousStatus) => {
     if (!pusher) {
         return Promise.resolve();
     }
-    return pusher.noteAsync(key, `Pokémon Go ${currentStatus}`, `Status change:\n${previousStatus} -> ${currentStatus}`)
+    return pusher.noteAsync(device, `Pokémon Go ${currentStatus}`, `Status change:\n${previousStatus} -> ${currentStatus}`)
         .catch(err => {
             console.warn(`error sending pushbullet message: ${err}`);
         });
